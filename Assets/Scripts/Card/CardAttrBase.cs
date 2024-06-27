0780729a6081c9c91950ab4751d3c82b71c3b0ec
翛然丶丶 <359222976@qq.com>
2024-06-27 11:52:52 +0800

    orange

--- Assets/Scripts/Card/CardAttrBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CardAttrBase : MonoBehaviour
-{
-    private float _damage;
-    private float _defend;
-}
