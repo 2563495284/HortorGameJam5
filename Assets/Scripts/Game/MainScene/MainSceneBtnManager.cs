df557f4547578ec6b6cd8534313a2acc3caa7de2
翛然丶丶 <359222976@qq.com>
2024-07-18 17:24:36 +0800

    字体

--- Assets/Scripts/Game/MainScene/MainSceneBtnManager.cs
+++ Assets/Scripts/Game/MainScene/MainSceneBtnManager.cs
@@ -15,7 +15,7 @@ public class MainSceneBtnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (PlayerModel.Instance.curtHero == null)
+        if (PlayerModel.Instance.role.heros.Count <= 0)
         {
             startBtn.enabled = false;
         }
